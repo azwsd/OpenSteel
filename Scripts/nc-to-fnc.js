7220d67d4cab8681e6d389e0fdf8712cfff2e862
azwsd <azwsdazwsd@gmail.com>
2025-06-09 09:22:11 +0300

    Added export DSTV to FNC functionality

--- Scripts/nc-to-fnc.js
+++ Scripts/nc-to-fnc.js
@@ -17,8 +17,7 @@ let webStartCut = '';
 let webEndCut = '';
 let flangeStartCut = '';
 let flangeEndCut = '';
-function ncLoadHeaderData(){
-    const fileData = filePairs.get(selectedFile);
+function ncLoadHeaderData(fileData){
     const splitFileData = fileData.split('\n');
     let lineCounter = 0;
     let isFirstIteration = true;
@@ -41,8 +40,8 @@ function ncLoadHeaderData(){
                 M.toast({html: 'File header contains an error!', classes: 'rounded toast-warning', displayLength: 2000});
                 break;
             }
-        //Removes \r from the end of string
-        line = line.replace(/\r$/, '');
+        //Removes \r from the end of string, replaces spaces with dashes, and removes leading and trailing spaces
+        line = line.trim().replace(/\s+/g, '-').replace(/\r$/, '');
 
         switch (lineCounter) {
             case 0:
@@ -108,9 +107,14 @@ const faceMapping = {
     'h': 'DC'   // horizontal face
 };
 
-function createHoleBlock() {
-    const fileData = filePairs.get(selectedFile);
+// Drill type mapping
+const drillTypeMapping = {
+    'Punch': 'TS11',
+    'Drill': 'TS31',
+    'Tap': 'TS41'
+};
 
+function createHoleBlock(fileData) {
     // String to hold all hole data
     let holeData = '';
     
@@ -150,7 +154,7 @@ function createHoleBlock() {
                 const FNCFace = faceMapping[face] || 'DB'; // default to DB if face not found
 
                 // Format the hole string
-                const holeString = `[HOL]   TS11   ${FNCFace}${diameter} X${xCoord} Y${yCoord}`;
+                const holeString = `[HOL]   ${drillTypeMapping[FNCDrillType]}   ${FNCFace}${diameter} X${xCoord} Y${yCoord}`;
 
                 // Add to global array
                 holeData += holeString + '\n';
@@ -170,25 +174,34 @@ function createMaterialBlock() {
 }
 
 function createPCSBlock() {
-    return `[[PCS]]\n[HEAD] C:${order} D:${drawing} N:${label}\nM:${steelQuality} CP:${profileCode} P:${profile}\nLP:${length} RAI${webStartCut} RAF${webEndCut} RBI${flangeStartCut} RBF${flangeEndCut}\nQ:${quantity}`;
+    return `[[PCS]]\n[HEAD] C:${order} D:${drawing} N:${label}\nM:${steelQuality} CP:${profileCode} P:${profile}\nLP${length} RAI${webStartCut} RAF${webEndCut} RBI${flangeStartCut} RBF${flangeEndCut}\nQI${quantity}`;
 }
 
-function createFNC() {
-    ncLoadHeaderData();
-    return `${createPRFBlock()}\n\n${createMaterialBlock()}\n\n${createPCSBlock()}\n${createHoleBlock()}`;
+function createFNC(fileData) {
+    ncLoadHeaderData(fileData);
+    return `${createPRFBlock()}\n\n${createMaterialBlock()}\n\n${createPCSBlock()}\n${createHoleBlock(fileData)}`;
 }
+
+let FNCDrillType = localStorage.getItem('FNCDrillType') || 'Punch'; // Default to 'Punch' if not set
 
 function ncToFnc() {
-    //M.Modal.getInstance(document.getElementById('exportModal')).close(); //Hide export modal
-
     // Check if a file is selected
     if (!selectedFile) {
         M.toast({html: 'No file selected!', classes: 'rounded toast-warning', displayLength: 2000});
         return;
     }
+    const fileData = filePairs.get(selectedFile);
+
+    if (!FNCDrillType) {
+        M.toast({html: 'Please select a drill type!', classes: 'rounded toast-warning', displayLength: 2000});
+        return;
+    }
+    const selectElement = document.getElementById('FNCDrillTypeSelect');
+    FNCDrillType = selectElement.value; // Get FNC drill type export value
+    localStorage.setItem('FNCDrillType', FNCDrillType); // Save the selected drill type to local storage
 
     // Create FNC content
-    const fncContent = createFNC();
+    const fncContent = createFNC(fileData);
 
     // Create a Blob with the output string
     const blob = new Blob([fncContent], { type: 'text/plain' });
@@ -201,4 +214,62 @@ function ncToFnc() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+
+    M.Modal.getInstance(document.getElementById('FNCModal')).close(); // Hide FNC export modal
 }
+
+function BatchNcToFnc() {
+    // Check if no files are loaded
+    if (filePairs.size === 0) {
+        M.toast({html: 'No files to export!', classes: 'rounded toast-error', displayLength: 2000});
+        return;
+    }
+
+    if (!FNCDrillType) {
+        M.toast({html: 'Please select a drill type!', classes: 'rounded toast-warning', displayLength: 2000});
+        return;
+    }
+
+    // Create new ZIP instance
+    const zip = new JSZip();
+    let processedCount = 0;
+
+    const selectElement = document.getElementById('FNCDrillTypeSelect');
+    FNCDrillType = selectElement.value; // Get FNC drill type export value
+    localStorage.setItem('FNCDrillType', FNCDrillType); // Save the selected drill type to local storage
+
+    for (const [key, value] of filePairs) {
+        const fileName = key.substring(0, key.lastIndexOf('.'));
+        const fncContent = createFNC(value);
+
+        // Add FNC content to ZIP file
+        zip.file(`${fileName}.fnc`, fncContent);
+        processedCount++;
+    }
+
+    // Generate ZIP file
+    zip.generateAsync({ type: 'blob' }).then(function(content) {
+        // Create a Blob with the ZIP content
+        const blob = new Blob([content], { type: 'application/zip' });
+        const url = URL.createObjectURL(blob);
+
+        let link = document.createElement('a');
+        link.href = url;
+        link.download = 'FNC_Export.zip'; // Name of the ZIP file
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+
+    M.Modal.getInstance(document.getElementById('FNCModal')).close(); // Hide FNC export modal
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+   const exportFNCButton = document.getElementById('exportFNCButton');
+   exportFNCButton.addEventListener('click', function() {
+        M.Modal.getInstance(document.getElementById('exportModal')).close(); // Hide export modal
+        const selectElement = document.getElementById('FNCDrillTypeSelect');
+        selectElement.value = FNCDrillType; // Set FNC drill type export value
+        M.FormSelect.init(selectElement); // Re-initialize to show the change
+   });
+});
