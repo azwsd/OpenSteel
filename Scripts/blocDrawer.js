977632a21d2a09cfc60b079c4d8e9354f4216e74
Ahmed Awad <ahmed.awad@energyasteel.com>
2025-06-28 08:10:04 +0300

    Reload whole file data when hole is added, to prevent redrawing bugs

--- Scripts/blocDrawer.js
+++ Scripts/blocDrawer.js
@@ -468,20 +468,9 @@ function addHole() {
     else holeLine = `BO\n  ${view}  ${xPos}${dimRef}  ${yPos}${holeType}  ${diameter}  ${depth}`;
     holeData.push([view, xPos, dimRef, yPos, holeType, diameter, depth, 'l', slotWidth, slotHeight, slotAngle]);
 
-    //Delete all holes for each view/layer
-    views.forEach(view => {
-        const layer = layers[view];
-        if (layer) {
-            const circles = layer.find(node => { return node.name() && node.name().startsWith('circle-'); }); //Find all circles in layer
-            circles.forEach(circle => circle.destroy()); //Destroy all circles in layer
-            layer.batchDraw(); //Redraw the layer
-        }
-    });
-
-    drawHoles(); //Redraw all holes including the newly added hole
-    document.getElementById('holeInfoContainer').innerHTML = ''; //Clears hole info container
     filePairs.set(selectedFile, filePairs.get(selectedFile).replace('EN', holeLine + '\nEN'));
-    addHoleData(); //Adds hole data to hole info tap
+
+    document.querySelector('#files .selected-file').click();
 }
 
 //Draws marks to the canvas
