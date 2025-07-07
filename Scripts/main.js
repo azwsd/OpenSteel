4080a45c3cff09aebf188785e245cb17016cf372
Ahmed Awad <ahmed.awad@energyasteel.com>
2025-07-07 07:52:33 +0300

    Added dxf to dstv modal to shortcutkey ignore

--- Scripts/main.js
+++ Scripts/main.js
@@ -211,7 +211,8 @@ function clickHoleData() {
 document.addEventListener('keydown', function (e) {
     if ( M.Modal.getInstance(document.getElementById('DXFModal')).isOpen ||
         M.Modal.getInstance(document.getElementById('createModal')).isOpen ||
-        M.Modal.getInstance(document.getElementById('addHoleModal')).isOpen) return; //Ignore key events if DXF/create modals is open
+        M.Modal.getInstance(document.getElementById('addHoleModal')).isOpen ||
+        M.Modal.getInstance(document.getElementById('dxfToNCModal')).isOpen) return; //Ignore key events if modals are open
     if (e.ctrlKey && e.key.toLowerCase() === 's') { //Detect Ctrl + S
         e.preventDefault(); //Prevent default browser save behavior
         downloadActiveViews();
